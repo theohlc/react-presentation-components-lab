58620ff37bf30eb2d2ef8c99931ad3799b930fec
theo <teddyhoover@gmail.com>
2021-04-21 13:56:24 -0500

    commit tree 45e0e856b8054003207aed9322bc9d8805b1b4df

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,28 @@
+import React, {Component} from "react";
+
 // Code SimpleComponent Here
+export default class SimpleComponent extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            mood: 'happy'
+        }
+    }
+
+    render() {
+        return(
+            <div onClick={this.handleClick}>
+                {this.state.mood}
+            </div>
+        )
+    }
+
+    handleClick = () => {
+        if (this.state.mood == 'happy') {
+            this.setState({mood: 'sad'})
+        } else {
+            this.setState({mood: 'happy'})
+        }
+    }
+}
